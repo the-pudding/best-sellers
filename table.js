1226ad63b20e7e9d15079baf7f6968286879a68f
russellgoldenberg <russellgoldenberg@gmail.com>
2017-05-08 15:28:58 -0400

    design

--- table.js
+++ table.js
@@ -1,4 +1,10 @@
 (function() {
+	var colors = ['#0C4B4A','#1D5560','#3A5C74','#5B6283','#7F658B','#A3678C','#C26B86','#DB717A','#EC7E6B']
+	var colorF = colors[0]
+	var colorM = colors[8]
+	// var colorFText = d3.color(colors[0]).brighter(10)
+	// var colorMText = d3.color(colors[8]).darker(10)
+
 	var tableData = null
 	var tableCols = ["name", "gender", "genre", "rank", "count"]
 	var chart = d3.select('.chart__tables')
@@ -27,12 +33,18 @@
 
 	function setupElements(){
 		var dataToPass = null
-		var tables = chart.selectAll("table")
+		var tables = chart.select('.tables__container').selectAll('table')
 				.data(tableData)
-				.enter()
+			.enter()
 				.append('table')
+				.attr('class', function(d) { return 'table-' + d.key })
+				.classed('is-active', function(d, i) { return !i })
+
 		tables.append('caption')
-			.text(function(d) {return d.key})
+			.text(function(d) {return 'Top authors in the ' + d.key + 's by number of books' })
+			.attr('class', 'x-small')
+
+		var tableToggle = chart.select('.tables__toggle')
 		var tableHead = tables.append('thead')
 		var tableBody = tables.append('tbody')
 
@@ -41,22 +53,60 @@
 				.enter()
 				.append('tr')
 
-		tableCols.map(function(k){
+		tableCols.map(function(k) {
 			tableHead.append('th')
 				.classed('is-number', function(d) { return k === 'rank' || k === 'count' })
 				.text(k)
 
-			tableRow.append('td')
+			var td = tableRow.append('td')
 				.classed('is-number', function(d) { return k === 'rank' || k === 'count' })
 				.text(function(d){ return d[k]})
+
+			if (k === 'name') {
+				// td.style('background-color', function(d) {
+				// 	return d.gender === 'Male' ? colorM : colorF;
+				// })
+				td.style('color', function(d) {
+					return d.gender === 'Male' ? colorM : colorF;
+				})
+				td.style('font-weight', 'bold')
+			}
+
+							
 		})
 
-//		tableHead.text(function(d){ return d.key })
+		var li = tableToggle.selectAll('li')
+			.data(tableData)
+		.enter().append('li')
+		
+		li.append('button')
+			.attr('class', 'btn toggle__button xxx-small')
+			.attr('value', function(d) { return d.key })
+			.text(function(d) { return d.key })
+			.classed('is-active', function(d, i) { return !i })
+	}
+
+	function handleToggle() {
+		chart.selectAll('.toggle__button')
+			.classed('is-active', false)
+
+		d3.select(this).classed('is-active', true)
+		
+		chart.selectAll('table')
+			.classed('is-active', false)
+
+		chart.select('.table-' + this.value)
+			.classed('is-active', true)
+	}
+
+	function setupEvents() {
+		chart.selectAll('.toggle__button').on('click', handleToggle)
 	}
 
 	function init() {
 		loadData(function() {
 			setupElements()
+			setupEvents()
 		})
 	}
 	init()
